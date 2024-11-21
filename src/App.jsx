323d2a293e896806c5247fa1e98229f27006d9ab
Tanmay Kalra <tanmaykalra0910@gmail.com>
2024-11-21 20:30:13 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import { Hero } from '../components/Hero'
+import { useState } from "react";
+import { Hero } from "../components/Hero";
+import { OurEvents } from "../components/OurEvents";
+import styled from "styled-components";
+
+const Main = styled.div`
+  min-height: 100vh;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Hero/>
+      <Hero />
+      <OurEvents />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
